283bc54a716d8ed99d4a746680ca9830dd6fb5ff
Pieter Hintjens <ph@imatix.com>
2005-01-18 15:40:09 +0000

    *** empty log message ***

--- clients/stdc/amqpcli_serial.c
+++ clients/stdc/amqpcli_serial.c
@@ -2,64 +2,193 @@
  *
  *  amqpcli_serial - AMQP/Fast prototype serial client
  *
- *  Copyright (c) 1991-2004 iMatix Corporation
- *  Copyright (c) 2004 JPMorgan Inc.
+ *  Sends 1000 messages in a batch and then reads them back in batches
+ *  of 100 with a commit after each 100 messages.
+ *
+ *  Copyright (c) 1991-2005 iMatix Corporation
+ *  Copyright (c) 2004-2005 JPMorgan Inc.
  *  -------------------------------------------------------------------------*/
 
 #include "amq_core.h"
 #include "amq_sclient.h"
+#include "version.h"
 
-#define CLIENT_VERSION "0.7"
+#define CLIENT_NAME "Serial test client - OpenAMQ/" VERSION "\n"
+#define NOWARRANTY \
+"This is free software; see the source for copying conditions.  There is NO\n" \
+"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
+#define USAGE                                                               \
+    "Syntax: clientname [options...]\n"                                     \
+    "Options:\n"                                                            \
+    "  -m number        Number of messages to send/receive (1000)\n"        \
+    "  -b batch         Size of each batch (100)\n"                         \
+    "  -x size          Size of each message (default = 1024)\n"            \
+    "  -s server        Name or address of server (localhost)\n"            \
+    "  -t level         Set trace level\n"                                  \
+    "  -q name          Queue to work with (test-small)\n"                  \
+    "  -p               Use persistent messages (no)\n"                     \
+    "                   0=none, 1=low, 2=medium, 3=high\n"                  \
+    "  -q               Quiet mode: no messages\n"                          \
+    "  -v               Show version information\n"                         \
+    "  -h               Show summary of command-line options\n"             \
+    "\nThe order of arguments is not important. Switches and filenames\n"   \
+    "are case sensitive. See documentation for detailed information.\n"
+
 
 int
 main (int argc, char *argv [])
 {
-    char
-        *hostname,
-        *clientname;
     int
-        consumer,
-        producer;
+        argn;                           /*  Argument number                  */
+    Bool
+        args_ok = TRUE,                 /*  Were the arguments okay?         */
+        quiet_mode = FALSE,             /*  -q means suppress messages       */
+        persistent = FALSE;             /*  Use persistent messages?         */
+    char
+        *opt_trace,                     /*  0-3                              */
+        *opt_messages,                  /*  Size of test set                 */
+        *opt_batch,                     /*  Size of batches                  */
+        *opt_server,                    /*  Host to connect to               */
+        *opt_msgsize,                   /*  Message size                     */
+        *opt_queue,                     /*  Queue to work with               */
+        **argparm;                      /*  Argument parameter to pick-up    */
     amq_sclient_t
         *amq_client;
     dbyte
         in_handle = 0,
         out_handle = 0;
+    int
+        messages,
+        batch_size,
+        msgsize,
+        count;
     amq_message_t
         *message;
+
+    /*  These are the arguments we may get on the command line               */
+    opt_trace    = "0";
+    opt_messages = "1000";
+    opt_batch    = "100";
+    opt_server   = "localhost";
+    opt_msgsize  = "1024";
+    opt_queue    = "test-small";
 
     console_send     (NULL, TRUE);
     console_capture  ("amqpcli_serial.log", 'w');
     console_set_mode (CONSOLE_DATETIME);
 
-    coprintf ("AMQP/Fast prototype client v" CLIENT_VERSION);
-    hostname   = argc > 1? argv [1]: "localhost";
-    clientname = argc > 2? argv [2]: "test client 1";
-    producer   = argc > 3? atoi (argv [3]): 1;
-    consumer   = argc > 4? atoi (argv [4]): 1;
+    argparm = NULL;                     /*  Argument parameter to pick-up    */
+    for (argn = 1; argn < argc; argn++) {
+        /*  If argparm is set, we have to collect an argument parameter      */
+        if (argparm) {
+            if (*argv [argn] != '-') {  /*  Parameter can't start with '-'   */
+                *argparm = strdupl (argv [argn]);
+                argparm = NULL;
+            }
+            else {
+                args_ok = FALSE;
+                break;
+            }
+        }
+        else
+        if (*argv [argn] == '-') {
+            switch (argv [argn][1]) {
+                /*  These switches take a parameter                          */
+                case 'm':
+                    argparm = &opt_messages;
+                    break;
+                case 'b':
+                    argparm = &opt_batch;
+                    break;
+                case 's':
+                    argparm = &opt_server;
+                    break;
+                case 't':
+                    argparm = &opt_trace;
+                    break;
+                case 'x':
+                    argparm = &opt_msgsize;
+                    break;
 
-    coprintf ("Connecting to %s using identity '%s'...", hostname, clientname);
-    amq_client = amq_sclient_new (clientname, "user", "pass");
+                /*  These switches have an immediate effect                  */
+                case 'p':
+                    persistent = TRUE;
+                    break;
+                case 'q':
+                    quiet_mode = TRUE;
+                    break;
+                case 'v':
+                    puts (CLIENT_NAME);
+                    puts (COPYRIGHT);
+                    puts (NOWARRANTY);
+                    puts (BUILDMODEL);
+                    puts ("Built on: " BUILDDATE);
+                    exit (EXIT_SUCCESS);
+                case 'h':
+                    puts (CLIENT_NAME);
+                    puts (COPYRIGHT);
+                    puts (NOWARRANTY);
+                    puts (USAGE);
+                    exit (EXIT_SUCCESS);
+
+                /*  Anything else is an error                                */
+                default:
+                    args_ok = FALSE;
+            }
+        }
+        else {
+            args_ok = FALSE;
+            break;
+        }
+    }
+    /*  If there was a missing parameter or an argument error, quit          */
+    if (argparm) {
+        coprintf ("Argument missing - type 'openamqd -h' for help");
+        goto failed;
+    }
+    else
+    if (!args_ok) {
+        coprintf ("Invalid arguments - type 'openamqd -h' for help");
+        goto failed;
+    }
+
+    messages   = atoi (opt_messages);
+    batch_size = atoi (opt_batch);
+    msgsize    = atoi (opt_msgsize);
+    if (batch_size > messages)
+        batch_size = messages;
+
+    amq_sclient_trace (atoi (opt_trace));
+
+    coprintf ("Connecting to %s...", opt_server);
+    amq_client = amq_sclient_new ("test client", "user", "pass");
+
     if (amq_client
-    &&  amq_sclient_connect (amq_client, hostname, "/test")) {
-        if (producer) {
-            out_handle = amq_sclient_producer (amq_client, "test-small");
+    &&  amq_sclient_connect (amq_client, opt_server, "/test")) {
+        out_handle = amq_sclient_producer (amq_client, opt_queue);
+        coprintf ("Sending %d messages to server...", messages);
+        for (count = 0; count < messages; count++) {
             message = amq_message_new ();
-            amq_message_testfill       (message, 4000);
-            amq_message_set_persistent (message, TRUE);
+            amq_message_testfill       (message, msgsize);
+            amq_message_set_persistent (message, persistent);
             amq_sclient_msg_send (amq_client, out_handle, message);
         }
-        if (consumer) {
-            in_handle = amq_sclient_consumer (amq_client, "test-small", 1, FALSE);
-            while (1) {
-                message = amq_sclient_msg_read (amq_client, 5000);
-                if (!message)
-                    break;
-                coprintf ("Message number %d arrived", amq_client->msg_number);
-                if (producer)
-                    amq_sclient_msg_send (amq_client, out_handle, message);
-                amq_sclient_msg_ack (amq_client);
+        in_handle = amq_sclient_consumer (amq_client, opt_queue, batch_size, FALSE);
+        coprintf ("Reading back messages...");
+        count = 0;
+        while (count < messages) {
+            message = amq_sclient_msg_read (amq_client, 0);
+            if (message) {
+                if (!quiet_mode)
+                    coprintf ("Message number %d arrived", amq_client->msg_number);
+                count++;
+                if (count % batch_size == 0) {
+                    amq_sclient_msg_ack (amq_client);
+                    amq_sclient_commit  (amq_client);
+                }
             }
+            else
+                break;
         }
         amq_sclient_close (amq_client, 0);
     }
@@ -69,5 +198,8 @@ main (int argc, char *argv [])
     coprintf ("Allocs=%ld frees=%ld\n", mem_allocs (), mem_frees ());
     icl_mem_assert ();
     return (0);
+
+    failed:
+        exit (EXIT_FAILURE);
 }
 
