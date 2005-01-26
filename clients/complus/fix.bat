a76042cf1d302fe548e32f5f17023be613b687ba
Martin Sustrik <sustrik@imatix.com>
2005-01-26 19:19:41 +0000

    *** empty log message ***

--- clients/complus/fix.bat
+++ clients/complus/fix.bat
@@ -1,13 +1,7 @@
 @echo off
-net stop "World Wide Web Publishing Service"
-net stop "IIS Admin Service"
 
-regsvr32 /u /s amq_client.dll
+regsvr32 /u /s amq.dll
 gsl   amq.gsl
-msdev amq.dsw /make
-regsvr32 /s amq_client.dll
-
-net start "IIS Admin Service"
-net start "World Wide Web Publishing Service"
-warble
+rem msdev amq.dsw /make
+regsvr32 /s amq.dll
 
