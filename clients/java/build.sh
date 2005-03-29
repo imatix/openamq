7beaf38c4696e4495d2c3645c65f90722a5d4f08
Gustavo Broos <gustavo@imatix.com>
2005-03-29 15:01:45 +0000

    No longer works.

--- clients/java/build.sh
+++
@@ -1,8 +0,0 @@
-#
-#   Build Java clients (provisional build script)
-#
-gsl amq_frames.xml
-lr amqpcli_serial.java.l
-javac com/imatix/openamq/framing/AMQFramingFactory.java
-javac amqpcli_serial.java
-javac EchoClient.java
