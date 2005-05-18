0e273c05a8462b74fddffb698b2d84cca8fb9642
Pieter Hintjens <ph@imatix.com>
2005-05-18 09:59:06 +0000

    *** empty log message ***

--- doc/builddot.bat
+++ doc/builddot.bat
@@ -4,4 +4,11 @@
 
 set FILE=%1
 set FILE=%FILE:.dot=%
+
+:-  Build PNG from dot file
 dot -Tpng %FILE%.dot -o%FILE%.png
+
+:- Build PDF from dot file
+dot -Tps2 %FILE%.dot -o%FILE%.ps
+epstopdf %FILE%.ps
+
