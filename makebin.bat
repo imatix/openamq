2e810b2e7fa4cf1fad441b9213c67bc616d769c4
Pieter Hintjens <ph@imatix.com>
2005-06-04 13:44:43 +0000

    Reorganising clients

--- makebin.bat
+++ makebin.bat
@@ -1,5 +1,5 @@
-@echo off
-set VERSION=0.8b1
+@echo on
+set VERSION=0.8d4
 set PACKAGE=openamq-%VERSION%-win32
 set HOME=c:\home
 
@@ -8,8 +8,8 @@ pause
 if exist %PACKAGE% rmdir/q/s %PACKAGE%
 mkdir %PACKAGE%
 
-xcopy /s/q/i %HOME%\openamq\clients %PACKAGE%\clients >nul
-xcopy /s/q/i %HOME%\openamq\servers %PACKAGE%\servers >nul
+xcopy /s/q/i %HOME%\openamq\clients %PACKAGE%\clients 
+xcopy /s/q/i %HOME%\openamq\servers %PACKAGE%\servers 
 
 zip -rqm %PACKAGE%.zip %PACKAGE%
 
