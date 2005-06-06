c993822d94f2efa8bbbf0379ca47b170a3ef0917
John Random <j.random.resource@jpmorgan.com>
2005-06-06 17:48:29 +0000

    Message abstraction.

---
+++ clients/java2/src/org/openamq/client/framing/AMQMessage.java
@@ -0,0 +1,62 @@
+package org.openamq.client.framing;
+
+import org.apache.mina.common.ByteBuffer;
+
+/**
+ * @author Robert Greig (robert.j.greig@jpmorgan.com)
+ */
+public class AMQMessage extends AMQFrame
+{
+    /* long integer */
+    public long bodySize;
+
+    public boolean persistent;
+    public byte priority;
+    /* long integer */
+    public long expiration;
+    /* short string */
+    public String mimeType;
+    /* short string */
+    public String encoding;
+    /* short string */
+    public String identifier;
+    public FieldTable headers;
+    public byte[] message;
+
+    public long getSize()
+    {
+        return 4 + 1 + 4 + EncodingUtils.encodedShortStringLength(mimeType) +
+               EncodingUtils.encodedShortStringLength(encoding) +
+               EncodingUtils.encodedShortStringLength(identifier) +
+               bodySize;
+    }
+
+    public void writePayload(ByteBuffer buffer)
+    {
+        EncodingUtils.writeUnsignedInteger(buffer, bodySize);
+        EncodingUtils.writeBoolean(buffer, persistent);
+        EncodingUtils.writeUnsignedByte(buffer, priority);
+        EncodingUtils.writeShortStringBytes(buffer, encoding);
+        EncodingUtils.writeShortStringBytes(buffer, identifier);
+        EncodingUtils.writeFieldTableBytes(buffer, headers);
+        if (message != null)
+        {
+            buffer.put(message);
+        }
+    }
+
+    public void populateFromBuffer(ByteBuffer buffer) throws AMQFrameDecodingException
+    {
+        bodySize = buffer.getUnsignedInt();
+        persistent = EncodingUtils.readBoolean(buffer);
+        priority = buffer.get();
+        encoding = EncodingUtils.readShortString(buffer);
+        identifier = EncodingUtils.readShortString(buffer);
+        headers = EncodingUtils.readFieldTable(buffer);
+        if (bodySize > 0)
+        {
+            message = new byte[(int)bodySize];
+            buffer.get(message, 0, (int)bodySize);
+        }
+    }
+}
