075ed4c2b34c4c81f02c147e9b56ffc3101b506a
Martin Lucina <mato@imatix.com>
2005-06-21 13:06:56 +0000

    Moving to openamq/clients_stdc

--- clients/agnostic/stdc/test_set.sh
+++
@@ -1,6 +0,0 @@
-#   This script runs various test sets
-#
-echo "Testing HANDLE QUERY... sending messages"
-test_level0 producer -h/test -dtest -mproducer -n5000 -i0
-echo "... sending query command"
-test_level0 query -h/test -dtest
