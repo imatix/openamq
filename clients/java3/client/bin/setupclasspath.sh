814649ca34e5ebdf76c9eade147dbdd4f3084e27
Robert Grieg <robert.j.greig@jpmorgan.com>
2005-08-15 14:19:47 +0000

    Fixed missing commons-collections.jar

--- clients/java3/client/bin/setupclasspath.sh
+++ clients/java3/client/bin/setupclasspath.sh
@@ -1,3 +1,3 @@
 LIBDIR=../../lib
 DISTDIR=../../dist
-CP=$LIBDIR/jms/jms.jar:$LIBDIR/logging-log4j/log4j-1.2.9.jar:$LIBDIR/mina/mina-0.7.3.jar:$LIBDIR/util-concurrent/backport-util-concurrent.jar:$DISTDIR/openamqj-common.jar:$DISTDIR/openamqj-jms.jar:$DISTDIR/openamqj-tests.jar
+CP=$LIBDIR/jms/jms.jar:$LIBDIR/logging-log4j/log4j-1.2.9.jar:$LIBDIR/mina/mina-0.7.3.jar:$LIBDIR/util-concurrent/backport-util-concurrent.jar:$LIBDIR/jakarta-commons/commons-collections-3.1.jar:$DISTDIR/openamqj-common.jar:$DISTDIR/openamqj-jms.jar:$DISTDIR/openamqj-tests.jar
