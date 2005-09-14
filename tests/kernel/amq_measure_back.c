7f58659d3533e9b191158824e97be54fd7479cd9
Tim Esselens <tim@imatix.com>
2005-09-14 15:32:54 +0000

    added 2 clients. one generates packets and sees if they are returned to itself another one passes packets back, using the reply_to field

---
+++ tests/kernel/amq_measure_back.c
@@ -0,0 +1,112 @@
+#include "asl.h"
+#include "amq_client_connection.h"
+#include "amq_client_session.h"
+
+int main (int argc, char** argv) 
+{
+    amq_client_connection_t
+        *connection = NULL;             //  Current connection
+    amq_client_session_t
+        *session = NULL;                //  Current session
+    
+    amq_content_jms_t
+        *message = NULL;                //  Current message
+    icl_longstr_t
+        *arguments = NULL;              //  Arguments to bind
+    byte
+        *payload = NULL;                //  The message payload
+
+    int
+        publish_result;                 //  Result of the message publication
+        
+    
+    //  Initialize system in order to use console
+    icl_system_initialise (argc, argv);
+
+    //  Set up a connection
+    connection = amq_client_connection_new ("localhost:9876",
+                                            amq_client_connection_auth_plain("guest","guest"),
+                                            0,      //  Trace level
+                                            30000); //  Timeout
+
+    if (connection) {
+        icl_console_print("I: connected to: %s/%s",
+                          connection->server_product,
+                          connection->server_version);
+    } else {
+        icl_console_print("E: could not connect to server"); 
+        goto finished;
+    }
+
+
+    //  Set up a session
+    session = amq_client_session_new (connection,"/");
+
+    if (!session) {
+        icl_console_print("E: could not open session to server");
+        goto finished;
+    }
+
+    //  Declare a queue
+    amq_client_session_queue_declare (session,      //  The session
+                                      0,            //  Access ticked granted by server
+                                      "global",     //  Queue scope
+                                      "service",    //  Queue name
+                                      FALSE,        //  Passive declaration
+                                      FALSE,        //  Durable declaration
+                                      FALSE,        //  Private queue
+                                      FALSE);       //  Auto delete
+
+   
+    //  Bind the queue to the exchange (named $queue)
+    arguments = asl_field_list_build ("destination", "service", NULL);
+    amq_client_session_queue_bind (session,
+                                   0,               //  Ticket
+                                   "global",        //  Queue scope
+                                   "service",       //  Queue
+                                   "$queue",        //  Exchange
+                                   arguments);      //  Arguments
+    icl_longstr_destroy (&arguments);
+
+    amq_client_session_jms_consume (session,
+        0,                          //  Access ticket granted by server
+        "global",                   //  Queue scope
+        "service",                  //  Queue name
+        0,                          //  Prefetch size
+        0,                          //  Prefetch count
+        FALSE,                      //  No local messages
+        TRUE,                       //  Auto-acknowledge
+        FALSE);                     //  Exclusive access to queue
+
+    for(;;) {
+        //XXX: why is there no output if you comment the line below?
+        amq_client_session_wait (session, 0);
+        message = amq_client_session_jms_arrived (session);
+        if(message) {
+            icl_console_print ("I: [%s] message {%s} arrived",
+                               "service",
+                               message->message_id);
+
+            amq_content_jms_possess(message); 
+            publish_result = amq_client_session_jms_publish (session,
+                                                             message,
+                                                             0,                 //  Ticket
+                                                             "$queue",          //  Exchange
+                                                             message->reply_to, //  Destination
+                                                             FALSE,             //  Mandatory
+                                                             FALSE);            //  Immediate
+        }
+        amq_content_jms_destroy (&message);
+
+    }
+
+    
+    //  Clean up
+    finished:
+    amq_client_session_destroy (&session);
+    amq_client_connection_destroy (&connection);
+
+    icl_mem_free (payload);
+    icl_system_terminate();
+    return 0;
+}
