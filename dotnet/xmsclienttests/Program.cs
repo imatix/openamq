7ffeb6935bd0021a1421e0ddf7d60a46ede49f29
Robert Grieg <robert.j.greig@jpmorgan.com>
2006-01-07 13:10:10 +0000

    Added in xmscommon framing utilities and infrastructure code

---
+++ dotnet/xmsclienttests/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xmsclienttests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
