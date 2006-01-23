07e8287ecffc6b3749b93a35ca036c81d4a97a04
Robert Grieg <robert.j.greig@jpmorgan.com>
2006-01-23 13:54:14 +0000

    AMQ-283: updated test

--- clients_java/client/bin/serviceProvidingClient.sh
+++ clients_java/client/bin/serviceProvidingClient.sh
@@ -3,4 +3,4 @@
 # sets up CP env var
 . ./setupclasspath.sh
 
-$JAVA_HOME/bin/java -cp $CP -Damqj.logging.level="INFO" org.openamq.requestreply1.ServiceProvidingClient $1 5672 guest guest /test serviceQ 
+$JAVA_HOME/bin/java -cp $CP -Damqj.logging.level="INFO" org.openamq.requestreply1.ServiceProvidingClient $1 guest guest /test serviceQ 
--- clients_java/client/bin/serviceRequestingClient.sh
+++ clients_java/client/bin/serviceRequestingClient.sh
@@ -3,7 +3,7 @@
 # sets up CP env var
 . ./setupclasspath.sh
 
-thehost=$1
+thehosts=$1
 shift
-echo $thehost
-$JAVA_HOME/bin/java -cp $CP -Damqj.logging.level="INFO" org.openamq.requestreply1.ServiceRequestingClient $thehost 5672 guest guest /test serviceQ "$@"
+echo $thehosts
+$JAVA_HOME/bin/java -cp $CP -Damqj.logging.level="INFO" org.openamq.requestreply1.ServiceRequestingClient $thehosts guest guest /test serviceQ "$@"
