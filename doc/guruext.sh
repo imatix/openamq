562ae2785c2b1f62d3f53a9f3e2d0311246e4c9c
Gustavo Broos <gustavo@imatix.com>
2006-02-13 10:03:11 +0000

    Jira:BASE40

---
+++ doc/guruext.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# generate PDF for gurudoc file
+
+set FILE=$1
+set FILE=$FILE.txt
+
+call mkgdl $FILE
+call gsl3 -quiet -tpl:latex_simple -gdl:$FILE gurudoc
+
+# twice, so TOC is correctly built
+pdflatex $FILE.tex
+pdflatex $FILE.tex
+
+# clean-up intermediary files
+del $FILE.aux $FILE.gdl $FILE.log $FILE.tex $FILE.toc
+
