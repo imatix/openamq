c67974e5c13c9f4a5bfb5dde4ee67aaaf49dee95
Robert Grieg <robert.j.greig@jpmorgan.com>
2006-03-15 15:11:13 +0000

    Moved .NET client to the Blaze project

--- dotnet/minadotnet/jpmorgan/mina/handler/demux/IMessageHandler.cs
+++
@@ -1,9 +0,0 @@
-using jpmorgan.mina.common;
-
-namespace jpmorgan.mina.handler.demux
-{
-    public interface IMessageHandler
-    {
-        void MessageReceived(ISession session, object message);
-    }
-}
--- dotnet/xmsclient/XMS/IMessageConsumer.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Text;
-
-namespace OpenAMQ.XMS
-{
-    public interface IMessageConsumer : IBM.XMS.IMessageConsumer
-    {
-    }
-}
