2bcb05be36d3a7d5a2661817e99eb93f1a0e22b6
Gustavo Broos <gustavo@imatix.com>
2006-07-04 10:40:52 +0000

    AMQPJ: Updated framing layer and project files

--- clients/java/test.sh
+++ clients/java/test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-lib=$IBASE/lib/java
+lib=$IBASE/amqpj/lib
+dist=$IBASE/amqpj/dist
 
-libs="$lib/mina-core-0.9.5-SNAPSHOT.jar:$lib/log4j-1.2.13.jar:bin"
+libs="$lib/mina-core-0.9.5-SNAPSHOT.jar:$lib/log4j-1.2.13.jar:$dist/amqpj.jar"
 
 $JAVA_HOME/bin/java -classpath "$libs:." org.openamq.Test
