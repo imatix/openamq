f79a4bdaaaf4f7e98ef3ed1c39e962537528e39a
Martin Lucina <mato@imatix.com>
2007-03-13 11:09:19 +0000

    Fix copyrights

--- clients/amq_client.c
+++ clients/amq_client.c
@@ -2,7 +2,7 @@
  *
  *  OpenAMQ client test program
  *
- *  Copyright (c) 1996-2007 iMatix Corporation
+ *  Copyright (c) 2007 iMatix Corporation
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
