daa8e246bd893c6774ba2210da9cc365e7a48208
Martin Lucina <mato@imatix.com>
2007-04-11 17:32:23 +0000

    Revert, try again with merged layout

--- java/client/bin/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# sets up CP env var
-. ./setupclasspath.sh
-
-#need the broker for some tests, but don't want to confuse existing tests...
-CP=$CP:../../broker/dist/openamqdj.jar
-
-$JAVA_HOME/bin/java -server -cp $CP -Damqj.logging.level="INFO" $*
--- java/client/src/org/openamq/jms/MessageConsumer.java
+++
@@ -1,7 +0,0 @@
-package org.openamq.jms;
-
-/** 
- */
-public interface MessageConsumer extends javax.jms.MessageConsumer
-{
-}
