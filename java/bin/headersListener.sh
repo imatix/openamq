95ccdf0f0f93b61871975078bb98ee2d92ca277b
Martin Lucina <mato@imatix.com>
2007-04-12 17:35:18 +0000

    First try for wrapper script

--- java/bin/headersListener.sh
+++ java/bin/headersListener.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+#  Launcher script for OpenAMQ Java test program
 
-# sets up CP env var
-. ./setupclasspath.sh
+#  Find Java and required JAR files
+if [ -n "$JAVA_HOME" ]; then
+    JAVA=$JAVA_HOME/bin/java
+else
+    JAVA=java
+fi
+if [ -n "$IBASE" -a -f "$IBASE/java/lib/openamq-java-launch.jar" ]; then
+    OPENAMQ_JAVA_HOME=$IBASE/java/lib
+fi
+if [ -f "../dist/openamq-java-launch.jar" ]; then
+    OPENAMQ_JAVA_HOME=../dist
+fi
+if [ -f "./dist/openamq-java-launch.jar" ]; then
+    OPENAMQ_JAVA_HOME=./dist
+fi
+if [ -n "$OPENAMQ_JAVA_HOME/openamq-java-launch.jar" ]; then
+    echo "Cannot locate openamq-java-launch.jar.  Please set either \$IBASE to"
+    echo "point to the IBASE where you installed the OpenAMQ Java Client, or"
+    echo "set \$OPENAMQ_JAVA_HOME to point to the top-level directory of the"
+    echo "OpenAMQ Java Client distribution."
+    exit 1
+fi
 
-$JAVA_HOME/bin/java -cp $CP -Damqj.logging.level="INFO" org.openamq.headers.Listener $*
+#  Execute the script
+exec $JAVA -cp $OPENAMQ_JAVA_HOME/openamq-java-launch.jar \
+      -Damqj.logging.level="INFO" \
+      org.openamq.headers.Listener $*
