0b7dbe4c9ddcc87ae8c6494e6d6ca653273da48a
Martin Lucina <mato@imatix.com>
2007-04-13 12:36:05 +0000

    Fix launcher script

--- java/bin/headersListener.sh
+++ java/bin/headersListener.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
-#  Launcher script for OpenAMQ Java test program
+#  Launcher script for OpenAMQ/JMS test program
 
 #  Find Java and required JAR files
+LAUNCHER_JAR=openamq-jms-launch.jar
 if [ -n "$JAVA_HOME" ]; then
     JAVA=$JAVA_HOME/bin/java
 else
     JAVA=java
 fi
-if [ -n "$IBASE" -a -f "$IBASE/java/lib/openamq-java-launch.jar" ]; then
+OPENAMQ_JAVA_HOME=
+if [ -n "$IBASE" -a -f "$IBASE/java/lib/$LAUNCHER_JAR" ]; then
     OPENAMQ_JAVA_HOME=$IBASE/java/lib
 fi
-if [ -f "../dist/openamq-java-launch.jar" ]; then
+if [ -f "../dist/$LAUNCHER_JAR" ]; then
     OPENAMQ_JAVA_HOME=../dist
 fi
-if [ -f "./dist/openamq-java-launch.jar" ]; then
+if [ -f "./dist/$LAUNCHER_JAR" ]; then
     OPENAMQ_JAVA_HOME=./dist
 fi
-if [ -n "$OPENAMQ_JAVA_HOME/openamq-java-launch.jar" ]; then
-    echo "Cannot locate openamq-java-launch.jar.  Please set either \$IBASE to"
-    echo "point to the IBASE where you installed the OpenAMQ Java Client, or"
-    echo "set \$OPENAMQ_JAVA_HOME to point to the top-level directory of the"
-    echo "OpenAMQ Java Client distribution."
+if [ ! -f "$OPENAMQ_JAVA_HOME/$LAUNCHER_JAR" ]; then
+    cat <<EOM
+Could not locate $LAUNCHER_JAR in any of:
+
+\$IBASE/java/lib/$LAUNCHER_JAR
+../dist/$LAUNCHER_JAR
+./dist/$LAUNCHER_JAR
+
+Please either set \$IBASE to point to the IBASE where you installed 
+OpenAMQ/JMS, or run this script from the top-level directory of the
+OpenAMQ/JMS distribution.
+EOM
     exit 1
 fi
 
-#  Execute the script
-exec $JAVA -cp $OPENAMQ_JAVA_HOME/openamq-java-launch.jar \
+#  Execute the test
+exec $JAVA -cp $OPENAMQ_JAVA_HOME/openamq-jms-launch.jar \
       -Damqj.logging.level="INFO" \
       org.openamq.headers.Listener $*
