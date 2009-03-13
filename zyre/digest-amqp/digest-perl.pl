9c4e211a4b1d6313836b8f4430db96a33441478e
Pieter Hintjens <ph@imatix.com>
2009-03-13 14:07:24 +0000

    RestMS and digest-amqp are moved to github

--- zyre/digest-amqp/digest-perl.pl
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/perl
-#
-use Digest::MD5;
-#   These arguments are passed on the command line
-my ($user, $realm, $password) = @ARGV;
-die "E: syntax: command user realm password\n" unless $password;
-print "digest = \"".Digest::MD5::md5_hex("$user:$realm:$password")."\"\n";
-
